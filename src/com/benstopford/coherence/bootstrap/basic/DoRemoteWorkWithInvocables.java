cd76365af1df069636f9e72d2a607c8f014617e5
Ben Stopford <benstopford@gmail.com>
2014-08-26 19:00:07 +0100

    added some comments

--- src/com/benstopford/coherence/bootstrap/basic/DoRemoteWorkWithInvocables.java
+++ src/com/benstopford/coherence/bootstrap/basic/DoRemoteWorkWithInvocables.java
@@ -18,18 +18,24 @@ public class DoRemoteWorkWithInvocables extends ClusterRunner {
 
     @Test
     public void shouldDoRemoteWork() throws InterruptedException {
+        //start two remote nodes
         startCoherenceProcess(config);
         startCoherenceProcess(config);
+
+        //get a handle on the invocation service
         InvocationService invocationService = (InvocationService) CacheFactory.getCacheFactoryBuilder()
                 .getConfigurableCacheFactory(config, classLoader)
                 .ensureService("MyInvocationService1");
+
         assertThat(CacheFactory.getCluster().getMemberSet().size(), is(3));
 
-        //this fella just returns the memberId but obviously could do something more useful
+        //create an invocable to do some work - this fella just returns the memberId of the node its running on
         SampleInvocable getMemberIdInvocable = new SampleInvocable();
 
+        //invoke!
         Map membersAndTheirIds = invocationService.query(getMemberIdInvocable, invocationService.getInfo().getServiceMembers());
 
+        //check that each of the three nodes returned their member id (one of them will be this process as we're running as a cluster member)
         assertThat(membersAndTheirIds.values(), (Matcher)hasItems(1,2,3));
     }
 }
